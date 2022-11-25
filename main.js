41d11066e1e8eaa1678010da50950286f8eae313
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2022-11-25 22:03:00 +0530

    feat: add draw-lines tutorial

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 import './style.css';
 
 import { spinning3DCube } from './js/spinning-cube';
+import { drawLines } from './js/draw-lines';
 
 // add tutorial progress here and mark isCurrent to the current tutorial,
 // add the tutorial's code in js/tutorial-title.js file
 // last active tutorial will be shown on page.
 const tutorialsMap = {
   spinningCube: {
-    isCurrent: true,
+    isCurrent: false,
     func: spinning3DCube,
+  },
+  drawLines: {
+    isCurrent: true,
+    func: drawLines,
   },
 };
 
 function main() {
-  console.log('Running tutorial');
   const tutorialsKeys = Object.keys(tutorialsMap);
   let currentTutorial;
+  let currentTutorialTitle;
 
   tutorialsKeys.forEach(tutorialKey => {
     const tutorial = tutorialsMap[tutorialKey];
     if (tutorial.isCurrent) {
       currentTutorial = tutorial;
+      currentTutorialTitle=tutorialKey;
     }
   });
 
   if (currentTutorial) {
+    console.log('Running tutorial', currentTutorialTitle);
     currentTutorial.func();
   } else {
     console.log('Noc current tutorial found.. Maybe all done?');
